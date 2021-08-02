d56337b631e72c57b1276cec17fc6816e63529a4
manismk <manismkmech@gmail.com>
2021-08-02 11:13:32 +0530

    Fix: Changed less size image and optimiaed some code

--- script.js
+++ script.js
@@ -12,17 +12,16 @@ stockForm.addEventListener("submit", (e) => {
   var quantityInt = parseInt(quantity.value);
   var currentAmount = parseFloat(current.value);
   returnAmount = currentAmount - purchaseAmount;
+  var percentage = (Math.abs(returnAmount) / purchaseAmount) * 100;
+  var amount = Math.abs(returnAmount) * quantityInt;
   //console.log(returnAmount);
   if (returnAmount < 0) {
     /*Loss*/
-    returnAmount = Math.abs(returnAmount);
-    var lossPercentage = (returnAmount / purchaseAmount) * 100;
-    var lossAmount = returnAmount * quantityInt;
-    result.innerText = `You lost ${lossPercentage.toFixed(
+    result.innerText = `You lost ${percentage.toFixed(
       2
-    )}%. Your total loss is Rs.${lossAmount.toFixed(2)}`;
+    )}%. Your total loss is Rs.${amount.toFixed(2)}`;
     result.style.color = "#DC2626";
-    if (lossPercentage >= 50) {
+    if (percentage >= 50) {
       image.setAttribute("src", "images/loss.webp");
       container.style.backgroundColor = "#FECACA";
     } else {
@@ -31,14 +30,11 @@ stockForm.addEventListener("submit", (e) => {
     }
   } else {
     /*Profit */
-    returnAmount = Math.abs(returnAmount);
-    var profitPercentage = (returnAmount / purchaseAmount) * 100;
-    var profitAmount = returnAmount * quantityInt;
-    result.innerText = `You gained ${profitPercentage.toFixed(
+    result.innerText = `You gained ${percentage.toFixed(
       2
-    )}%. Your total profit is Rs.${profitAmount.toFixed(2)}`;
+    )}%. Your total profit is Rs.${amount.toFixed(2)}`;
     result.style.color = "#059669";
-    if (profitPercentage >= 50) {
+    if (percentage >= 50) {
       image.setAttribute("src", "images/profit.webp");
       container.style.backgroundColor = "#A7F3D0";
     } else {
