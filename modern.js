dc9df95e11581791577788f8c1001c58087f5c7c
manismk <manismkmech@gmail.com>
2021-08-12 22:04:45 +0530

    update:added invested- current calculation

--- modern.js
+++ modern.js
@@ -5,6 +5,10 @@ var quantity = document.querySelector("#quantity");
 var sliderCurrentInput = document.querySelector("#currentInput");
 var current = document.querySelector("#current");
 
+var xValues = ["Invested", "Current"];
+var yValues = [1, 1];
+var barColors = ["#b91d47", "#00aba9"];
+
 sliderPurchaseInput.oninput = function () {
   updateslider(sliderPurchaseInput, 10000, purchase);
 };
@@ -14,28 +18,37 @@ sliderQuantityInput.oninput = function () {
 sliderCurrentInput.oninput = function () {
   updateslider(sliderCurrentInput, 10000, current);
 };
+window.onload = function () {
+  updateslider();
+};
 
 function updateslider(slider, maxValue, spanName) {
-  var value = slider.value;
-  spanName.innerText = slider.value;
-  value = (value / maxValue) * 100;
+  if (slider) {
+    var value = slider.value;
+    spanName.innerText = slider.value;
+    value = (value / maxValue) * 100;
 
-  slider.style.background = `linear-gradient(to right, red 0%, red ${value}%, grey ${value}%, grey 100%)`;
+    slider.style.background = `linear-gradient(to right, red 0%, red ${value}%, grey ${value}%, grey 100%)`;
+  }
+
+  var purchaseValue = sliderPurchaseInput.value;
+  var quantityValue = sliderQuantityInput.value;
+  var currentValue = sliderCurrentInput.value;
+  var purchaseAmount = purchaseValue * quantityValue;
+  var currentAmount = currentValue * quantityValue;
+
+  yValues = [purchaseAmount, currentAmount];
+
+  new Chart("myChart", {
+    type: "doughnut",
+    data: {
+      labels: xValues,
+      datasets: [
+        {
+          backgroundColor: barColors,
+          data: yValues,
+        },
+      ],
+    },
+  });
 }
-
-var xValues = ["Italy", "France"];
-var yValues = [55, 49];
-var barColors = ["#b91d47", "#00aba9"];
-
-new Chart("myChart", {
-  type: "doughnut",
-  data: {
-    labels: xValues,
-    datasets: [
-      {
-        backgroundColor: barColors,
-        data: yValues,
-      },
-    ],
-  },
-});
